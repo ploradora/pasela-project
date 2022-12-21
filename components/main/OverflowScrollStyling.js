36745871387b57c65dde84a769a472af81e9e678
Sebastian S <suciu.seb@gmail.com>
2022-12-21 20:32:28 +0200

    grid height fixed

--- components/main/OverflowScrollStyling.js
+++ components/main/OverflowScrollStyling.js
@@ -3,7 +3,7 @@ import React from "react";
 const OverflowScrollStyling = (props) => {
   return (
     <div
-      className={`pr-3 scrollbar-thin scrollbar-thumb-dashboard-border scrollbar-thumb-rounded-full scrollbar-track-transparent overflow-y-scroll max-h-[calc(100vh_-_260px)] min-h-[calc(100vh_-_260px)] ${props.stylingOnMusicTab}`}
+      className={`pr-3 scrollbar-thin scrollbar-thumb-dashboard-border scrollbar-thumb-rounded-full scrollbar-track-transparent overflow-y-scroll max-h-[calc(100vh_-_262px)] min-h-[calc(100vh_-_262px)] ${props.stylingOnMusicTab}`}
     >
       {props.children}
     </div>
