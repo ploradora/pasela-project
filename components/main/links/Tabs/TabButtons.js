7e4b22309446144172cbe6d1cf22068a32aaf8c8
Sebastian S <suciu.seb@gmail.com>
2023-01-11 13:21:59 +0200

    Update TabButtons.js

--- components/main/links/Tabs/TabButtons.js
+++ components/main/links/Tabs/TabButtons.js
@@ -3,20 +3,22 @@ import { useContext } from "react";
 
 const TabList = () => {
   const ctx = useContext(TabContext);
-  
+
   return (
     <>
-      {ctx.selectContent.map((btn) => (
-        <button
-          key={btn.id}
-          onClick={() => ctx.setCurrentTab(btn.val)}
-          className={`overflow-hidden relative text-center text-l text-grey font-bold py-4 px-12 rounded-t-lg hover:text-dark after:absolute after:bg-white after:h-full after:w-full after:top-0 after:left-0 after:-z-10 after:translate-y-full after:transition-all hover:after:transition-all ${
-            btn.val === ctx.currentTab ? "after:translate-y-0 text-darker" : ""
-          }`}
-        >
-          {btn.title}
-        </button>
-      ))}
+      {ctx.selectContent.map((btn) => {
+        const isActiveClass =
+          btn.val === ctx.currentTab ? "after:translate-y-0 text-darker" : "";
+        return (
+          <button
+            key={btn.id}
+            onClick={() => ctx.setCurrentTab(btn.val)}
+            className={`overflow-hidden relative text-center text-l text-grey font-bold py-4 px-12 rounded-t-lg hover:text-dark after:absolute after:bg-white after:h-full after:w-full after:top-0 after:left-0 after:-z-10 after:translate-y-full after:transition-all hover:after:transition-all ${isActiveClass}`}
+          >
+            {btn.title}
+          </button>
+        );
+      })}
     </>
   );
 };
